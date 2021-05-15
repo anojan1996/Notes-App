f51f93f75e824fa094fdc805556bc5be0965c789
anojan1996 <anojan2012@gmail.com>
2021-05-15 20:38:31 +0530

    Message Flashing

--- Notes/routes.py
+++ Notes/routes.py
@@ -1,12 +1,12 @@
 from Notes import app
-from flask import render_template
+from flask import render_template, request, flash
 
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
@@ -14,6 +14,23 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account Created!', category='success')
     return render_template('sign_up.html')
