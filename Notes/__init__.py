f2f44f778810b443910b305a27510adce82f5df0
anojan1996 <anojan2012@gmail.com>
2021-05-16 00:20:42 +0530

    Database Models

--- Notes/__init__.py
+++ Notes/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
 app.config['SECRET_KEY'] = 'Notes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+
 
 from Notes import routes
 
