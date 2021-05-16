6a7753e44530ac277ade286bf3c8a7b0a470fd22
anojan1996 <anojan2012@gmail.com>
2021-05-16 11:45:23 +0530

    Logging in users

--- Notes/models.py
+++ Notes/models.py
@@ -10,9 +10,11 @@ class User(db.Model, UserMixin):
     notes = db.relationship('Note')
 
 
-class Note(db.model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeginKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+db.create_all()
 
--- Notes/routes.py
+++ Notes/routes.py
@@ -1,5 +1,7 @@
-from Notes import app
-from flask import render_template, request, flash
+from Notes import app, db
+from flask import render_template, request, flash, redirect
+from Notes.models import User, Note
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route('/home')
@@ -8,6 +10,19 @@ def home():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect('/home')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -18,19 +33,25 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First Name must be greater than 1 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!', category='success')
+            return redirect('/home')
     return render_template('sign_up.html')
